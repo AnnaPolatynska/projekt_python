253a20b48a08a0c364844b316373b23712a4a424
Anna_Polatynska <anna.polatynska@gmail.com>
2017-08-14 12:15:49 +0200

    dodanie_f-cji_pracownik

--- praca_dieta_box.py
+++ praca_dieta_box.py
@@ -10,10 +10,11 @@ import pymysql
 
 
 # interfejs dla pracownika wprowadzajego do bazy danych dieta_box dane klientów
+# logowanie przez dane pracowników z MySQL np. login: 'w_kepinski', hasło: 'Kepin1!'
 class MySQLConnector:
     def __init__(self, passwd):
         self.conn = pymysql.connect("localhost", "root2", "MySQL13", "dieta_box", charset='utf8mb4')
-        cur = conn.cursor()        
+        cur = conn.cursor()
         login = str(input("Podaj login: "))
         haslo = str(input("Podaj hasło: "))        
         bdd = "SELECT login, haslo FROM uprawnienia_prac WHERE login=%s and haslo=%s"
@@ -22,42 +23,118 @@ class MySQLConnector:
         print("Połączenie ustanowione")
         if(cur.rowcount == 1):
             print("Zalogowano poprawnie")
-            nav = ''
-            while(nav != "Q"):
-                nav = input("Co chcesz zrobić? (S)-select, (I)-insert, (U)-update, (Q)- wyjście")
-                if(nav == "S"):
-                    self.select()
-                elif(nav == "I"):
-                    self.insert()
-                elif(nav == "U"):
-                    self.update()                
-            print("Połączenie zakończone")
-            self.conn.close() 
-                           
-            
+            nav = input("Co chcesz zrobić? (K)-baza klientów, (Z)-baza zamówień, (Q)- wyjście")
+            while(nav != "K" or nav != "k" or nav != "Z" or nav != "z" or nav != "q" or nav != "Q"):
+                nav = input("Co chcesz dalej wybrać? (K)-baza klientów, (Z)-baza zamówień, (Q)- wyjście")
+                if(nav == "K" or nav == "k"):
+                    self.selectKlient()
+                elif(nav == "Z"or nav == "z"):
+                    self.selectZamowienia()
+                elif(nav == "Q"or nav == "q"):
+                    print("Połączenie zakończone")
+                    self.conn.close()
         else:
+            print("---UWAGA---")  
             print("Błąd logowania")
 
-  
-    def select(self):
+
+#menu praca na kliencie     
+    def selectKlient(self):             
+        navk = input("Co chcesz zrobić z danymi klienta? (S)-select, (I)-insert, (U)-update, (Q)- wyjście")
+        if(navk == "S" or navk == "s"):
+            self.selectS()
+        elif(navk == "I"or navk == "i"):
+            self.insertI()
+        elif(navk == "U"or navk == "u"):
+            self.updateU()    
+        elif(navk == "Q"or navk == "q"):
+            print("Połączenie zakończone")
+            self.conn.close() 
+        else:
+            print("------------------UWAGA------------------")  
+            print("Wprowadzono niepoprawny kod, wybierz ponownie: ")  
+            print(" ")             
+   
+#menu praca na zamówieniu 
+    def selectZamowienia(self):             
+        navz = input("Co chcesz zrobić z zamówieniem ? (T)-tabela zamówień, (D)-dopisz zamówienie, (N)-nadpisz brakujące dane w zamówieniu, (Q)- wyjście")
+        if(navz == "T" or navz == "t"):
+            self.selectT()
+        elif(navz == "D"or navz == "d"):
+            self.insertD()
+        elif(navz == "N"or navz == "n"):
+            self.updateN() 
+        elif(navz == "Q"or navz == "q"):
+            print("Połączenie zakończone")
+            self.conn.close() 
+        else:
+            print("------------------UWAGA------------------")  
+            print("Wprowadzono niepoprawny kod, wybierz ponownie: ")  
+            print(" ")
+       
+
+    # Zamówienia możliwość podejrzenia i poprawy zamówień uszeregowanych wg daty realizacji
+    def selectT(self):
+        self.c.execute("SELECT opłacono, id_d, dieta, od_kiedy, DATE_ADD(od_kiedy, INTERVAL 1 MONTH) AS do_kiedy, id_k, imię, nazwisko, id_o, nazwa_punktu, adres_punktu FROM diety NATURAL LEFT JOIN zamówienia NATURAL LEFT JOIN klient NATURAL LEFT JOIN punkty_odbioru WHERE opłacono IS NOT NULL ORDER BY od_kiedy;")
+        res = self.c.fetchall()        
+        print("%-8s | %-10s | %-30s | %-15s | %-15s | %-10s | %-15s | %-15s | %-10s | %-20s | %-15s" % ("opłata", "id diety", "dieta", "od kiedy", "do kiedy", "id kl.", "imię", "nazwisko", "id odb.", "nazwa punktu", "adres punktu"))       
+        print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+        for v in res:
+            opłacono =v [0]
+            id_d=v [1]
+            dieta=v [2]
+            od_kiedy=v [3]
+            do_kiedy=v [4]
+            id_k=v [5]
+            imie = v[6]
+            nazwisko = v[7]
+            id_o = v[8]
+            nazwa_punktu = v[9]
+            adres_punktu = v[10]
+            print("%-8s | %-10s | %-30s | %-15s | %-15s | %-10s | %-15s | %-15s | %-10s | %-20s | %-15s" %  (opłacono, id_d, dieta, od_kiedy, do_kiedy, id_k, imie, nazwisko, id_o, nazwa_punktu, adres_punktu))
+    
+   
+# baza zamówień- dopisanie nowego zamówienia w przypadku składania zamówienia telefonicznego przez klienta
+    def insertD(self):
+        self.c.execute("INSERT INTO zamówienia VALUES (8, 6, 4, 'NIE', '2017-11-01');")
+        self.conn.commit()
+        print("wprowadzono dane: id zamówienia:8, id klienta:6, id diety:4, opłacono:NIE, od kiedy: 2017-11-01")
+# baza zamówień- nadpisanie np statusu płatności zamówienia w przypadku opłaty zamówienia przez klienta   
+    def updateN(self):
+        self.c.execute("UPDATE zamówienia SET opłacono='TAK' where id_k = 8;")
+        self.conn.commit()
+        print("dane dotyczące płatności zaktualizowane")  
+
+
+   
+
+# baza klientów - lista
+    def selectS(self):
         self.c.execute("select id_k, imię, nazwisko, telefon, miasto, e_mail FROM klient NATURAL LEFT JOIN uprawnienia;")
         res = self.c.fetchall()        
-        print("%-3s %-15s %-15s %-15s %-15s %-15s" % ("id", "imię", "nazwisko", "telefon", "miasto", "e_mail"))       
+        print("%-3s %-15s %-15s %-15s %-15s %-15s" % ("id klienta", "nazwisko", "imię", "telefon", "miasto", "id odbioru"))       
         print("---------------------------------------------------------------------------------------------------------------------------")
         for v in res:
             id_k =v [0]
-            imie = v[1]
-            nazwisko = v[2]
+            nazwisko = v[1]
+            imie = v[2]
             telefon = v[3]
             miasto = v[4]
-            e_mail = v[5]
-            print("%-3s %-15s %-15s %-15s %-15s %-15s" % (id_k, imie, nazwisko, telefon, miasto, e_mail))
-    def insert(self):
-        self.c.execute("INSERT INTO klient VALUES (10, 'Malinowski','Zygmunt', '612-198-424','Piastów')")
+            id_o = v[5]
+            print("%-3s %-15s %-15s %-15s %-15s %-15s" % (id_k, nazwisko, imie, telefon, miasto, id_o))
+  
+  
+
+# baza klientów - dopisanie nowego klienta
+    def insertI(self):
+        self.c.execute("INSERT INTO klient VALUES (10, 'Malinowski','Zygmunt', '612-198-424','Piastów', 2);")
+        self.c.execute("INSERT INTO uprawnienia VALUES (12, 10, 'Zygmunt', 'Malinowski', 'klient', 'Malina@o2.pl', 'H_malinowski', 'Mali1!' );")
         self.conn.commit()
-        print("wprowadzono dane: 10,  Zygmunt, Malinowski, 612-198-424, Piastów")
-    def update(self):
-        self.c.execute("UPDATE klient SET imię='Zbigniew' where nazwisko = 'Malinowski';")
+        print("wprowadzono dane: id klienta:11,  Imię: Zygmunt, Nazwisko: Malinowski, telefon: 512-198-789,, Miasto: Piastów, id punktu: 2")
+        print("wprowadzono dane: ID uprawnień: 13, uprawnienia: klient, e_mail:Malina@o2.pl, login: H_malinowski, haslo: Mali1!")
+# baza klientów - zaktualizowanie istniejącego klienta        
+    def updateU(self):
+        self.c.execute("UPDATE klient SET imię='Henryk' where nazwisko = 'Malinowski';")
         self.conn.commit()
         print("dane zaktualizowane")        
 
--- praca_dieta_box_klient.py
+++ praca_dieta_box_klient.py
@@ -5,11 +5,11 @@ import pymysql
 # zainportowanie bazy danych
 conn = pymysql.connect("localhost", "root2", "MySQL13", "dieta_box", charset='utf8mb4')
 import pymysql
-
-
 # interfejs dla klientów wyboru lokalizacji odbioru i dietaBoxu
 
-#menu główne
+#menu główne logowanie klienta 
+#wybierz dowolny login i hasło dla klienta np. login: 'a_dzida', hasło: 'Dzida1!'
+
 class MySQLConnector:
     def __init__(self, passwd):
         self.conn = pymysql.connect("localhost", "root2", "MySQL13", "dieta_box", charset='utf8mb4')
@@ -20,38 +20,44 @@ class MySQLConnector:
         cur.execute(bdd,(login,haslo))            
         self.c = self.conn.cursor()
         print("Połączenie ustanowione")
+         
         if(cur.rowcount == 1):
             print("Zalogowano poprawnie")
-            nav_s = ''
-            while(nav_s != "Q"):
-                nav_s = input("Co chcesz zrobić? (P)-Wybrać punkty odbioru, (O)-zapoznać się z ofertą, (K)-kupić dietę, (Q)- wyjście")
-                if(nav_s == "P"):
-                    self.selectP()
-                elif(nav_s == "O"):
+            nav_s = input("Co chcesz zrobić? (Z)-Zobaczyć punkty odbioru, (O)-opisy diet, (K)-kupić dietę, (Q)- powrót")
+            while(nav_s != "Q" or nav_s != "q" or nav_s != "Z" or nav_s != "z" or nav_s != "O" or nav_s != "o" or nav_s != "K" or nav_s != "k"):
+                nav_s = input("Co chcesz dalej wybrać? (Z)-Zobaczyć punkty odbioru, (O)-opisy diet, (K)-kupić dietę, (Q)- powrót");
+                if(nav_s == "Z" or nav_s == "z"):
+                    self.selectZ()
+                elif(nav_s == "O" or nav_s == "o"):
                     self.selectO()
-                elif(nav_s == "K"):
-                    self.selectK()                
-            print("Połączenie zakończone")
-            self.conn.close()    
+                elif(nav_s == "K" or nav_s == "k"):
+                    self.selectK()
+                elif(nav_s == "Q"or nav_s == "q"):
+                    print("Połączenie zakończone")
+                    self.conn.close()      
             
         else:
-            print("Błąd logowania")
-        
-   
-# adresy punktów odbioru        
-    def selectP(self):
-        self.c.execute("SELECT nazwa_punktu, adres_punktu, miasto FROM punkty_odbioru ORDER BY miasto;")
+            print("---UWAGA---")  
+            print("Błąd logowania")                
+  
+# (Z)-Zobaczyć punkty odbioru = adresy punktów odbioru        
+    def selectZ(self):
+        self.c.execute("SELECT id_o, nazwa_punktu, adres_punktu, miasto FROM punkty_odbioru;")
         res = self.c.fetchall() 
-        print("%-30s   |   %-30s   |   %-30s " % ("punkt odbioru", "adres punktu odbioru", "miasto"))
-        print("----------------------------------------------------------------------------------------------------")        
+        print("%-10s   |   %-35s   |   %-30s   |   %-30s " % ("Id punktu", "klub sprotowy z punktem odbioru", "adres punktu odbioru", "miasto"))
+        print("------------------------------------------------------------------------------------------------------------------------")        
         for p in res:
-            nazwa_punktu = p[0]
-            adres_punktu = p[1]
-            miasto = p[2]
-            print("%-30s   |   %-30s   |   %-30s" % (nazwa_punktu, adres_punktu, miasto))
-        print("----------------------------------------------------------------------------------------------------")     
+            id_o = p[0]
+            nazwa_punktu = p[1]
+            adres_punktu = p[2]
+            miasto = p[3]
+            print("%-10s   |   %-35s   |   %-30s   |   %-30s" % (id_o, nazwa_punktu, adres_punktu, miasto))
+        print("------------------------------------------------------------------------------------------------------------------------")     
 
-# opisy diet
+   
+
+#  (O)-zapoznać się z ofertą = opisy diet
+   # opisy diet
     def selectO(self):
         self.c.execute("SELECT dieta, opis, cena_netto * 1.23 AS 'cena_brutto' FROM diety;")
         res = self.c.fetchall()        
@@ -63,45 +69,49 @@ class MySQLConnector:
             cena_brutto = o[2]         
             print("%-30s   |   %-140s   |   %-30s " %  (dieta, opis, cena_brutto))
         print("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
-    
 
 
 
-
-# wybór diet
+# (K) Kupno diety
     def selectK(self):
-        nav_k = ''
-        while(nav_k != "Q"):
-            nav_k = input("Jaką dietę chcesz kupić? (1) dietę odchudzającą-egzotyczne smaki , (2) dietę odchudzjącą standardową, (3) dietę zdrowotną- wzmocnij serce, (4) dietę zdrowotną standardową, (5) dietę sportową - wytrzymałość, (6) dietę sportową - siła (Q)- wyjście")
-            if(nav_k == "1"):
-                self.select1()
-                print("zamówiłeś dietę odchudzającą-egzotyczne smaki")
-                print(" ")
-            elif(nav_k == "2"):
-                self.select2()
-                print("Zamówiłeś dietę odchudzjącą standardową. Twoje zamówienie czeka na opłatę.")
-                print(" ")
-            elif(nav_k == "3"):
-                self.select3()
-                print("Zamówiłeś dietę zdrowotną- wzmocnij serce. Twoje zamówienie czeka na opłatę.")
-                print(" ")
-            elif(nav_k == "4"):
-                self.select4()
-                print("Zamówiłeś dietę zdrowotną standardową.Twoje zamówienie czeka na opłatę.")
-                print(" ")
-            elif(nav_k == "5"):
-                self.select5()
-                print("Zamówiłeś dietę dietę sportową - wytrzymałość. Twoje zamówienie czeka na opłatę.")
-                print(" ")
-            elif(nav_k == "6"):
-                self.select6()
-                print("Zamówiłeś dietę dietę sportową - siła. Twoje zamówienie czeka na opłatę.")
-                print(" ")
- 
-                
-        print("Połączenie zakończone")
-        self.conn.close() 
-     
+        nav_k = input("Jaką dietę chcesz kupić? (1) dietę odchudzającą-egzotyczne smaki , (2) dietę odchudzjącą standardową, (3) dietę zdrowotną- wzmocnij serce, (4) dietę zdrowotną standardową, (5) dietę sportową - wytrzymałość, (6) dietę sportową - siła (Q)- wyjście")
+        if(nav_k == "1"):
+            self.select1()
+            print("zamówiłeś dietę odchudzającą-egzotyczne smaki")
+            print(" ")
+        elif(nav_k == "2"):
+            self.select2()
+            print("Zamówiłeś dietę odchudzjącą standardową. Twoje zamówienie czeka na opłatę.")
+            print(" ")
+        elif(nav_k == "3"):
+            self.select3()
+            print("Zamówiłeś dietę zdrowotną- wzmocnij serce. Twoje zamówienie czeka na opłatę.")
+            print(" ")
+        elif(nav_k == "4"):
+            self.select4()
+            print("Zamówiłeś dietę zdrowotną standardową.Twoje zamówienie czeka na opłatę.")
+            print(" ")
+        elif(nav_k == "5"):
+            self.select5()
+            print("Zamówiłeś dietę dietę sportową - wytrzymałość. Twoje zamówienie czeka na opłatę.")
+            print(" ")
+        elif(nav_k == "6"):
+            self.select5()
+            print("Zamówiłeś dietę dietę sportową - siła. Twoje zamówienie czeka na opłatę.")
+            print(" ")    
+                        
+        elif(nav_k == "Q" or nav_k == "q"):
+            print("Połączenie zakończone")
+            self.conn.close() 
+        else:
+            print("------------------UWAGA------------------")  
+            print("Wprowadzono niepoprawny kod, wybierz ponownie: ")  
+            print(" ")                 
+
+
+
+
+    # (1-6) Wybór poszczególnych diet    
     def select1(self):
         self.c.execute("SELECT dieta, cena_netto, cena_netto * 1.23 AS 'cena_brutto' FROM diety WHERE id_d=1;")
         res = self.c.fetchall()        
@@ -112,8 +122,8 @@ class MySQLConnector:
             print("%-30s   |   %-40s   |   %-30s " % ("dieta", "cena netto", "cena brutto"))
             print("---------------------------------------------------------------------------------------------------------------------------")
             print("%-30s   |   %-40s   |   %-30s " %  (dieta, cena_netto, cena_brutto))
-            print("---------------------------------------------------------------------------------------------------------------------------")
-                
+            print(" ")       
+
     def select2(self):
         self.c.execute("SELECT dieta, cena_netto, cena_netto * 1.23 AS 'cena_brutto' FROM diety WHERE id_d=2;")
         res = self.c.fetchall()        
@@ -124,8 +134,7 @@ class MySQLConnector:
             print("%-30s   |   %-40s   |   %-30s " % ("dieta", "cena netto", "cena brutto"))
             print("---------------------------------------------------------------------------------------------------------------------------")
             print("%-30s   |   %-40s   |   %-30s " %  (dieta, cena_netto, cena_brutto))
-            print("---------------------------------------------------------------------------------------------------------------------------")        
-
+            print(" ")       
     def select3(self):
         self.c.execute("SELECT dieta, cena_netto, cena_netto * 1.23 AS 'cena_brutto' FROM diety WHERE id_d=3;")
         res = self.c.fetchall()        
@@ -136,7 +145,7 @@ class MySQLConnector:
             print("%-30s   |   %-40s   |   %-30s " % ("dieta", "cena netto", "cena brutto"))
             print("---------------------------------------------------------------------------------------------------------------------------")
             print("%-30s   |   %-40s   |   %-30s " %  (dieta, cena_netto, cena_brutto))
-            print("---------------------------------------------------------------------------------------------------------------------------")       
+            print(" ")         
 
     def select4(self):
         self.c.execute("SELECT dieta, cena_netto, cena_netto * 1.23 AS 'cena_brutto' FROM diety WHERE id_d=4;")
@@ -148,7 +157,7 @@ class MySQLConnector:
             print("%-30s   |   %-40s   |   %-30s " % ("dieta", "cena netto", "cena brutto"))
             print("---------------------------------------------------------------------------------------------------------------------------")
             print("%-30s   |   %-40s   |   %-30s " %  (dieta, cena_netto, cena_brutto))
-            print("---------------------------------------------------------------------------------------------------------------------------")       
+            print(" ")            
 
     def select5(self):
         self.c.execute("SELECT dieta, cena_netto, cena_netto * 1.23 AS 'cena_brutto' FROM diety WHERE id_d=5;")
@@ -160,11 +169,12 @@ class MySQLConnector:
             print("%-30s   |   %-40s   |   %-30s " % ("dieta", "cena netto", "cena brutto"))
             print("---------------------------------------------------------------------------------------------------------------------------")
             print("%-30s   |   %-40s   |   %-30s " %  (dieta, cena_netto, cena_brutto))
-            print("---------------------------------------------------------------------------------------------------------------------------")       
+            print(" ")        
 
     def select6(self):
-        self.c.execute("SELECT dieta, cena_netto, cena_netto * 1.23 AS 'cena_brutto' FROM diety WHERE id_d=6;")
-        res = self.c.fetchall()        
+        self.c.execute ("INSERT INTO zamówienia VALUES (8, 9 ,6 , 0, 'NIE','12-11-2017';")
+        res = self.c.fetchall()   
+              
         for a in res:
             dieta = a[0]
             cena_netto = a[1]
@@ -172,8 +182,84 @@ class MySQLConnector:
             print("%-30s   |   %-40s   |   %-30s " % ("dieta", "cena netto", "cena brutto"))
             print("---------------------------------------------------------------------------------------------------------------------------")
             print("%-30s   |   %-40s   |   %-30s " %  (dieta, cena_netto, cena_brutto))
-            print("---------------------------------------------------------------------------------------------------------------------------")       
+            print(" ")       
+            print("wprowadzono dane zamówienia:  ")  
+            
+ 
+            
+            
 c1= MySQLConnector("MySQL13")   
-  
+
+
+
 
  
+     
+     
+     
+     
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+    # DO POPRAWKI
+    #(W)-Wybrać punkty odbioru = wybór punktu odbioru
+        def selectW(self):
+            nav_w = input("Jaki punt odbioru wybierasz? podaj id punktu (1)-Klub Sportowy S4 (Warszawa), (2)-Szkoła tańca FEN, (3)-Klub Sportowy (S4 Piastów), (4)-Oxygen Fitness, (5)-Cross Klub Ursus, (6)-Fight Klub Pitbul, (Q)- wyjście do menu głównego")
+            while(nav_w != "Q" or nav_w != "q"):
+                print("------------------UWAGA------------------")  
+                print("Wprowadzono niepoprawny kod, wybierz ponownie: ")  
+                # print(" ") 
+                nav_w = input("Jaki punt odbioru wybierasz? podaj id punktu (1-6)")
+                if(nav_w == "1"):
+                    self.select1()
+                    print("wybrałeś punkt: " + "(SELECT 'nazwa_punktu', 'adres_punktu', 'miasto' FROM punkty_odbioru WHERE 'id_o'=1 ;)")
+                    print(" ")
+                elif(nav_w == "2"):
+                    self.select2()
+                    print("wybrałeś punkt: " + "(SELECT 'nazwa_punktu', 'adres_punktu', 'miasto' FROM punkty_odbioru WHERE 'id_o'=2 ;)")
+                    print(" ")
+                elif(nav_w == "3"):
+                    self.select3()
+                    print("wybrałeś punkt: " + "(SELECT 'nazwa_punktu', 'adres_punktu', 'miasto' FROM punkty_odbioru WHERE 'id_o'=3 ;)")
+                    print(" ")
+                elif(nav_w == "4"):
+                    self.select4()
+                    print("wybrałeś punkt: " + "(SELECT 'nazwa_punktu', 'adres_punktu', 'miasto' FROM punkty_odbioru WHERE 'id_o'=4 ;)")
+                    print(" ")
+                elif(nav_w == "5"):
+                    self.select5()
+                    print("wybrałeś punkt: " + "(SELECT 'nazwa_punktu', 'adres_punktu', 'miasto' FROM punkty_odbioru WHERE 'id_o'=5 ;)")
+                    print(" ")
+                elif(nav_w == "6"):
+                    self.select6()
+                    print("wybrałeś punkt: ")
+                    print("SELECT 'nazwa_punktu', 'adres_punktu', 'miasto' FROM punkty_odbioru WHERE 'id_o'=6 ;)")
+                    self.c.execute("INSERT INTO zamówienia VALUES (8, 10, wyb_id_d, 6, 'NIE', 'NIE WYBRANO')")
+                              
+    
+                    print("Połączenie zakończone")
+                self.conn.close()    
+           
+    
+    
+    
+    
+       # def insertOdb(self):
+      #      self.c.execute("INSERT INTO zamówienia VALUES (null, id_k, null, id_o, opłacono, od_kiedy)")
+        #    self.conn.commit()
+         #   print("wybrałeś punkt odbioru: " + id_o+ id) 
+          #  nav_s = ''
+"""
